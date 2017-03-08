aa22444d5387fa9937ee34f7e1f7d0237d1436d9
Neil Ferner <n.e.ferner@gmail.com>
2017-03-08 18:53:20 -0500

    connect html and css, begin building functions

---
+++ rpnCalc.js
@@ -0,0 +1,28 @@
+createStack(){
+  let stack = [];
+
+  push(element){
+    stack.push(element);
+  };
+
+  pop(){
+    stack.pop();
+  };
+
+  peek(){
+    return stack[stack.length-1];
+  };
+}
+
+rpnCalc(var1, var2, operator){
+  let stack = [];
+  let var1 = '';
+  let var2 = '';
+  let operator = + || - || / || *;
+
+  if (stack = [])
+    
+  stack.push(var1)
+
+
+}
